0de1d813eec65076a597e239c7235dde8a989a0f
InnaKr <49992989+InnaKr@users.noreply.github.com>
2019-05-13 23:27:10 +0300

    Create 4-2 numbers.py

---
+++ 4-2 numbers.py
@@ -0,0 +1,11 @@
+S="ds0df123ds d4fs5d 6s7d sd89sd"
+#S=S.split()
+print(S)
+def Num(S):
+    N=[]
+    for i in S:
+        if i.isdigit():
+            N=N+i
+            print (N)
+    return (N)
+print(Num(S))
