51522c6ff4e234fd3b02dc032ff1474ff8a9498f
InnaKr <49992989+InnaKr@users.noreply.github.com>
2019-05-22 22:21:42 +0300

    Create 7-1 Mix.py

---
+++ 7-1 Mix.py
@@ -0,0 +1,19 @@
+N = int(input("Введіть ціле число: "))
+
+def K(a):
+    P, R = [], []
+    for _ in range(a):
+        S = input("Введіть числовий ряд: ")
+        G = [float(el) for el in S.split()]
+        for k in G:
+            if k.is_integer():
+                P.append(k)
+            else:
+                R.append(k)
+    P.sort()
+    R.sort()
+    P.extend(R)
+
+    print(P)
+
+K(N)
