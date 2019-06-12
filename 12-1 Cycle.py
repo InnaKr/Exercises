79277084916f72d6e685f99ce5d5cb34702cc36a
InnaKr <49992989+InnaKr@users.noreply.github.com>
2019-06-12 22:23:42 +0300

    Update 12-1 Cycle.py

--- 12-1 Cycle.py
+++ 12-1 Cycle.py
@@ -1,9 +1,9 @@
 def cycle(*iterable):
-    for i in iterable:
-        for l in i:
-            yield l
+    while True:
+        for i in iterable:
+            for l in i:
+                yield l
 
 
-while True:
-    for i in cycle('AB', 'CD'):
-        print(i)
+for i in cycle('AB', 'CD'):
+    print(i)
